68d90c39171464ed6601506b4df77b517747bd59
qatesterM <cachorroterrier1@gmail.com>
2023-06-01 11:02:23 -0300

    commit inicial

--- script2.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function () {
-        document.querySelector(".nav-links").
-            classList.toggle("nav-links-responsive")
-    })
